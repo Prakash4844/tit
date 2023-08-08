73f27de46d1adc9df6e773005b2eff35dd6b161e
Prakash4844 <pk484442@gmail.com>
2023-08-08 09:07:18 +0530

    * Help option is shown on     - No options provided     - `-h | --help`
    * Version is Shown in Help option and separately using:
        - `-v | --version`

--- name_subject_to_change/cli/main.py
+++ name_subject_to_change/cli/main.py
@@ -1,47 +1,30 @@
 import typer
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
 
-app = typer.Typer(no_args_is_help=True, help=f"ZAQ - Version: {__version__}")
-
-
-def vers(value: bool):
-    """
-    Report current version of ZAQ - Version Control System package.
-    :param value: bool
-    """
-    if value:
-        typer.echo(f"ZAQ Version: {__version__}")
-        raise typer.Exit()
+app = typer.Typer(
+    name="zaq",
+    no_args_is_help=True,
+    add_completion=False,
+    invoke_without_command=True,
+    help=f"ZAQ: Version Control System. \n\nVersion: {__version__}",
+    context_settings={"help_option_names": ["-h", "--help"]},
+)
 
 
 @app.callback(invoke_without_command=True)
-def app_callback():
-    """
-    Report current version of ZAQ - Version Control System package.
-    :return:
-    """
-    typer.echo(f"ZAQ Version: {__version__}")
-    raise typer.Exit()
-
-
-@app.command()
-def zaq():
-    print(f"Zaq is the temporary name of this project.")
-
-
-@app.callback()
-def main(
-        version: bool = typer.Option(
-            None,
-            "-v",
-            "--version",
-            callback=app_callback,
-            is_eager=True,
-            help="Report current version of zaq package.",
-        )
-):
-    return
+def get_version(
+    version: bool = typer.Option(
+        False,
+        "--version",
+        "-v",
+        help="Print version and exit.",
+    ),
+) -> None:
+    """Display the version."""
+    if version:
+        typer.echo(f"ZAQ Version: {__version__}")
+        raise typer.Exit()
 
 
 if __name__ == "__main__":
