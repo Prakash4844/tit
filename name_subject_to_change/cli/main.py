e49d2dc3e63ff833f0ebae697a5a098a65f8c030
Prakash4844 <pk484442@gmail.com>
2023-08-08 07:52:44 +0530

    * version can be shown now, using `-v | --version` * help message is invoked by default **Bonus** Help message shows version to when invoked without argument.
    Completed #1 and working on #2

--- name_subject_to_change/cli/main.py
+++ name_subject_to_change/cli/main.py
@@ -1,35 +1,47 @@
 import typer
 
-app = typer.Typer()
+__version__ = "0.1.0"
+
+app = typer.Typer(no_args_is_help=True, help=f"ZAQ - Version: {__version__}")
 
 
-def version_callback(value: bool):
+def vers(value: bool):
     """
     Report current version of ZAQ - Version Control System package.
+    :param value: bool
     """
-    __version__ = "0.1.0"
     if value:
         typer.echo(f"ZAQ Version: {__version__}")
         raise typer.Exit()
 
 
-@app.callback()
-def main(
-    version: bool = typer.Option(
-        None,
-        "-v",
-        "--version",
-        callback=version_callback,
-        is_eager=True,
-        help="Report current version of zaq package.",
-    )
-):
-    return
+@app.callback(invoke_without_command=True)
+def app_callback():
+    """
+    Report current version of ZAQ - Version Control System package.
+    :return:
+    """
+    typer.echo(f"ZAQ Version: {__version__}")
+    raise typer.Exit()
 
 
 @app.command()
 def zaq():
     print(f"Zaq is the temporary name of this project.")
+
+
+@app.callback()
+def main(
+        version: bool = typer.Option(
+            None,
+            "-v",
+            "--version",
+            callback=app_callback,
+            is_eager=True,
+            help="Report current version of zaq package.",
+        )
+):
+    return
 
 
 if __name__ == "__main__":
